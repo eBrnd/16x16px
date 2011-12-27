24059fb5e91fe87345fb206446a4072284a6b14d
eBrnd <github@ebrnd.de>
2011-12-27 16:06:18 +0100

    Background modulo scrolling

--- background.cpp
+++ background.cpp
@@ -22,9 +22,15 @@ bool Background::loadFile(std::string filename)
     return false;
 }
 
-void Background::draw(int x, int y) // TODO make bg wrap around
+void Background::draw(int x, int y)
 {
-  SDL_Rect bg_pos = { x, y, 320, 240 }; // TODO make width + height variable (window size)
+  SDL_Rect bg_pos = { x % bg_surf->w, y % bg_surf->h, 320, 240 }; // TODO make width + height variable (window size)
   SDL_Rect dst_pos = { 0, 0, 320, 240 };
   SDL_BlitSurface(bg_surf, &bg_pos, display, &dst_pos);
+  if((x % bg_surf->w) + 320 > bg_surf->w) // fill in the rest when we're scrolling "between" instances of the bg
+  { // TODO make this work for vertical scrolling as well
+    SDL_Rect bg_fill_pos = { 0, y % bg_surf->h, 320, 240 };
+    SDL_Rect fill_dst = { bg_surf->w - x, 0, 320, 240 };
+    SDL_BlitSurface(bg_surf, &bg_fill_pos, display, &fill_dst);
+  }
 }
