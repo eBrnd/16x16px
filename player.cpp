04b6e7202332fca6a8081e5e0e3e1eee80c936a1
eBrnd <github@ebrnd.de>
2011-12-27 22:18:14 +0100

    removed horizontal collision glitch; implemented vertical collision with ceiling

--- player.cpp
+++ player.cpp
@@ -38,9 +38,15 @@ void Player::input(Uint8 direction)
 {
   // moving left and right
   if(direction & 0x04)
+  {
     vx -= 16;
+    faceRight = false;
+  }
   if(direction & 0x08)
+  {
     vx += 16;
+    faceRight = true;
+  }
   if(!direction)
     vx = 0;
   if(vx > 64)
@@ -49,7 +55,7 @@ void Player::input(Uint8 direction)
     vx = -64;
   // test for collision
   while(foreground->collision((px+vx)/16, py/16))
-    vx--;
+    faceRight ? vx-- : vx++;
   px += vx;
 
   if(direction & 0x01 && jumping < 4)
@@ -65,8 +71,13 @@ void Player::physics()
   vy = vy >= 64 ? 64 : vy + 16;
   while(foreground->collision(px/16, (py+vy)/16))
   {
-    jumping = 0;
-    vy--;
+    if(vy > 0)
+    {
+      jumping = 0;
+      vy--;
+    } else {
+      vy = 8;
+    }
   }
   py += vy;
 }
